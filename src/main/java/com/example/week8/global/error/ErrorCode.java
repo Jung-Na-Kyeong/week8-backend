2f4f69b64ccd7442302391564765a6cb5d141f51
Jung-Na_Kyeong <jan022655@naver.com>
2026-05-31 01:28:42 +0900

    fix: ë ErrorCod에 ë ALREADY_EXIST_NICKNAME 추가

--- src/main/java/com/example/week8/global/error/ErrorCode.java
+++ src/main/java/com/example/week8/global/error/ErrorCode.java
@@ -14,10 +14,11 @@ public enum ErrorCode {
 
     // 401 Unauthorized
     UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 일치하지 않습니다.", "UNAUTHORIZED_USER"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.", "INVALID_TOKEN"), //
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.", "INVALID_TOKEN"),
 
     // 409 Conflict
-    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일입니다.", "ALREADY_EXIST_EMAIL"), //
+    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일입니다.", "ALREADY_EXIST_EMAIL"),
+    ALREADY_EXIST_NICKNAME(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다.", "ALREADY_EXIST_NICKNAME"),
 
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", "INTERNAL_SERVER_ERROR");
